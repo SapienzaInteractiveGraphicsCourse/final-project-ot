88e57f180610cf5ddd3c475b25ae62a51a9d228f
Francesco Tassone <francescor.tassone@gmail.com>
2021-07-16 19:25:24 +0200

    Config and Utilities

---
+++ engine/Config.js
@@ -0,0 +1,9 @@
+export class Config {
+    static world_width = 10;
+    static world_height = 10;
+    static world_depth = 10;
+
+
+
+
+}
---
+++ engine/Utilities.js
@@ -0,0 +1,17 @@
+export class Utilities {
+    static world_to_render(coordinates_or_x,y,z){
+        let x = coordinates_or_x;
+        if (arguments.length === 1) {
+            z = coordinates_or_x[2];
+            y = coordinates_or_x[1];
+            x = coordinates_or_x[0];
+        }
+        var w = Config.world_width;
+        var h = Config.world_height;
+        var d = Config.world_depth;
+        return [x - w/2 + 0.5, y - h/2 + 0.5, z - d/2 + 0.5];
+    }
+
+    static get_
+
+}
